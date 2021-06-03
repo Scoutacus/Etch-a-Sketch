d71ceaae6dcf5e8c218d4358eb64b683ed709632
Scoutacus <vanguardacus@gmail.com>
2021-06-03 16:40:53 -0400

    Created a button to replace the eleventy-billion divs I created that dynamically creates the divs for me. Feels like progress on this project is slow goin'. Also updated styles.css

--- script.js
+++ script.js
@@ -1,34 +1,5 @@
+function addElement(){
+    var item = document.createElement("div");
 
-let div = document.createElement("div")
-let div2 = document.createElement("div")
-let div3 = document.createElement("div")
-let div4 = document.createElement("div")
-let div5 = document.createElement("div")
-let div6 = document.createElement("div")
-let div7 = document.createElement("div")
-let div8 = document.createElement("div")
-let div9 = document.createElement("div")
-let div10 = document.createElement("div")
-let div11 = document.createElement("div")
-let div12 = document.createElement("div")
-let div13 = document.createElement("div")
-let div14 = document.createElement("div")
-let div15 = document.createElement("div")
-let div16 = document.createElement("div")
-
-document.getElementById("container").appendChild(div);
-document.getElementById("container").appendChild(div2);
-document.getElementById("container").appendChild(div3);
-document.getElementById("container").appendChild(div4);
-document.getElementById("container").appendChild(div5);
-document.getElementById("container").appendChild(div6);
-document.getElementById("container").appendChild(div7);
-document.getElementById("container").appendChild(div8);
-document.getElementById("container").appendChild(div9);
-document.getElementById("container").appendChild(div10);
-document.getElementById("container").appendChild(div11);
-document.getElementById("container").appendChild(div12);
-document.getElementById("container").appendChild(div13);
-document.getElementById("container").appendChild(div14);
-document.getElementById("container").appendChild(div15);
-document.getElementById("container").appendChild(div16);
+    document.getElementById("container").appendChild(item);
+}
